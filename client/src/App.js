08e42e997cca75b17f0b8c9167ecb9eaf8c31545
Tay Kong Yu <kongyutay@gmail.com>
2024-05-22 22:49:24 +0800

    Almost finish with the socket io, got bug

--- client/src/App.js
+++ client/src/App.js
@@ -1,20 +1,30 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import './app.css';
 import Card from './components/card/Card';
 import Navbar from './components/navbar/Navbar';
-import { posts } from "./data"
+import { posts } from "./data";
+import { io } from "socket.io-client";
 
 function App() {
   const [ username, setUsername ] = useState("");
   const [ user, setUser ] = useState("");
+  const [ socket, setSocket] = useState(null);
+
+  useEffect(() => {
+    setSocket(io("http://localhost:5000"));
+  }, []);
+
+  useEffect(() => {
+    socket?.emit("newUser", user)
+  }, [socket,user]);
 
   return (
     <div className='container'>
       { user ? (
         <>
-          <Navbar />
+          <Navbar socket={socket} />
           {posts.map((post) => (
-            <Card key={post.id} post={post}></Card>
+            <Card key={post.id} post={post} socket={socket} user={user} />
           ))}
           <span className="username">{user}</span>
         </>
--- client/src/components/card/Card.jsx
+++ client/src/components/card/Card.jsx
@@ -7,9 +7,16 @@ import Share from "../../img/share.svg";
 import Info from "../../img/info.svg";
 import { useState } from "react";
 
-const Card = ({post}) => {
+const Card = ({ post, socket, user }) => {
     const [liked, setLiked] = useState(false);
-    const handleNotification = () => setLiked(true);
+    const handleNotification = (type) => {
+        setLiked(true);
+        socket.emit("sendNotification", {
+            senderName: user,
+            receiverName: post.username,
+            type,
+        })
+    };
 
     return (
         <div className="card">
@@ -22,11 +29,11 @@ const Card = ({post}) => {
                 {liked ? (
                     <img src={HeartFilled} alt="" className="cardIcon" />
                 ) : (
-                    <img src={Heart} alt="" className="cardIcon" onClick={handleNotification}/>
+                    <img src={Heart} alt="" className="cardIcon" onClick={()=>handleNotification(1)}/>
                 )}
                 
-                <img src={Comment} alt="" className="cardIcon" />
-                <img src={Share} alt="" className="cardIcon" />
+                <img src={Comment} alt="" className="cardIcon" onClick={()=>handleNotification(2)}/>
+                <img src={Share} alt="" className="cardIcon" onClick={()=>handleNotification(3)}/>
                 <img src={Info} alt="" className="cardIcon infoIcon" />
             </div>
         </div>
